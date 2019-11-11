9550b47f8490bd3364ff500223b2b7c0cb85de3d
ramonrodrigoss@gmail.com <qmermama666>
2019-11-11 16:13:56 -0300

    Numeros Pares e numeros Impares

---
+++ ex047.py
@@ -0,0 +1,6 @@
+print(('Amostra dos numeros pares'))
+for c in range(0 , 51, 2):
+    print(c, end=' ')
+print('Esses são os numeros pares do 0 até o 50 \nE esses são os numeros impares')
+for d in range(1 , 51 , 2):
+    print(d, end=" ")
