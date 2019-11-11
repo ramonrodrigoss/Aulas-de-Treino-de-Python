8ccc085b4d12786cbe29589bdf8aa642a0d89f7b
ramonrodrigoss@gmail.com <qmermama666>
2019-11-11 13:54:11 -0300

    programa de gerenciamento de pagamento

--- ex044.py
+++ ex044.py
@@ -9,20 +9,20 @@ opção = int(input ("""FORMAS DE PAGAMENTO
 Qual é a opção?
 """))
 if opção == 1:
-    print('Oi')
+    print('O Valor da compra é de R${:.2f} e com o desconto de 10% fica R${:.2f}'.format(preço,preço-(preço*10/100)))
 
 elif opção == 2:\
-    print('Oi')
+    print('O Valor da compra é de R${} e com o desconto de 5% fica R${}'.format(preço,preço-(preço*5/100)))
 
 elif opção == 3:
-    print()
+    print('O Valor da compra é de R${}, nesssa forma de pagamento não tem desconto'.format(preço))
+    print('As parcelas ficam no valor de R${}'.format(preço/2))
 
 elif opção == 4:
     parcela = int(input('Quantas parcelas?'))
-    juros = (preço*20)/100
-    print(juros)
-#    print('Sua compra será parcelada em {}x de R${:.2f} COM JUROS'.format(parcela,((preço/parcela)*20)/100))
- #   print('Sua compra de R${:.2f} vai custar R${:.2f} no final'.format(preço,(preço+(preço*20)/100)))
+    juros = (preço+(preço*20)/100)/parcela
+    print('Sua compra será parcelada em {}x de R${:.2f} COM JUROS'.format(parcela,juros))
+    print('Sua compra de R${:.2f} vai custar R${:.2f} no final'.format(preço,(preço+(preço*20)/100)))
 
 else:
     print('OPÇÃO INVALIDA')
