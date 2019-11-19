1569d56c738fe896062a7e4acf9426811b60398e
Ramonrodrigoss <ramonrodrigoss@gmail.com>
2019-11-19 15:33:17 -0300

    Programa que mostra se um numero é primo e quais seus divisores

---
+++ ex053.py
@@ -0,0 +1,13 @@
+print('Verificador de Palindromo')
+frase = str(input('Digite uma frase: ')).strip().upper()
+palavra = frase.split()
+junto =''.join(palavra)
+inverso = ''
+
+for letra in range(len(junto)-1,-1,-1):
+    inverso += junto[letra]
+print('O inverso de {} é {}'.format(junto,inverso))
+if inverso == junto:
+    print('Temos um Palíndromo!')
+else:
+    print('Não temos um Palíndromo')
